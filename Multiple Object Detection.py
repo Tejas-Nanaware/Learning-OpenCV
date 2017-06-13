bfb5e7a4fceaaa51e2958338ec7d9b79b576183f
Tejas-Nanaware <nanawaret@hotmail.com>
2017-06-13 12:44:43 +0530

    added blurs to reduce noise

--- Multiple Object Detection.py
+++ Multiple Object Detection.py
@@ -30,8 +30,20 @@ while True:
 
 	res = cv2.bitwise_and(frame, frame, mask = mask)
 
+	# Adding blurs to reduce noise
+	#creating a smoothed kernel
+	kernel = np.ones((15,15), np.float32) / 225
+	smoothed = cv2.filter2D(res, -1, kernel)
+	# Gaussian Blur
+	blur = cv2.GaussianBlur(res, (15,15), 0)
+	# Median Blur
+	median_blur = cv2.medianBlur(res, 15) #this is really nice
+	# Bilateral Blur
+	bilateral = cv2.bilateralFilter(res, 15, 75, 75)
+
 	cv2.imshow('frame', frame)
 	cv2.imshow('res', res)
+	cv2.imshow('blur', bilateral)
 	
 	k = cv2.waitKey(5) & 0xFF
 	if k == 27:
